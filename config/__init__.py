4d55dd53ee448671f5c0464b4aa158b74e0d3157
K4ZED <kenzaathallah.wijaya@gmail.com>
2025-11-20 10:39:07 +0700

    masih error bagian login

--- config/__init__.py
+++ config/__init__.py
@@ -1,6 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class Config:
-    MYSQL_HOST = "localhost"
-    MYSQL_USER = "root"
-    MYSQL_PASSWORD = "Knk9250563#"
-    MYSQL_DATABASE = "shop"
-    SECRET_KEY = "rahasia-waow-2025"
+    MYSQL_HOST = os.getenv("MYSQL_HOST")
+    MYSQL_USER = os.getenv("MYSQL_USER")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+    MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
+    SECRET_KEY = os.getenv("SECRET_KEY")
